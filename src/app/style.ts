2b3b7a4a15bc2bcc2ab468a688bb7e4d0eea5613
heloisacrisilva <heloisacristovao02@gmail.com>
2026-03-31 10:42:40 -0300

    feat: setup

---
+++ src/app/style.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 4.75rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const LogoContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.h1`
+  text-align: center;
+  margin: 1.75rem;
+  line-height: 150%;
+
+  @media (max-width: 580px) {
+    font-size: 1.4rem;
+  }
+`
+
+export const InfoContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  @media (max-width: 580px) {
+    flex-direction: column;
+  }
+`
