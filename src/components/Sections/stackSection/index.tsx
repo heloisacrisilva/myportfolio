42e84be068f44724640b6b30a937ab6c2257ca8a
heloisacrisilva <heloisacristovao02@gmail.com>
2026-04-04 17:54:51 -0300

    chore: stack icon color

--- src/components/Sections/stackSection/index.tsx
+++ src/components/Sections/stackSection/index.tsx
@@ -5,6 +5,8 @@ import * as S from './style';
 import StackCard from '@/components/Cards/StackCard';
 import { SiGooglecloud, SiNodedotjs, SiFigma, SiPostgresql, SiGit } from 'react-icons/si';
 import { RiJavaFill, RiJavascriptFill, RiNextjsLine } from 'react-icons/ri';
+import { use } from 'react';
+import { useTheme } from '@/contexts/themeProviders';
 
 interface StackSectionProps {
   lang: string;
@@ -12,47 +14,48 @@ interface StackSectionProps {
 
 export const StackSection = ({ lang }: StackSectionProps) => {
   const t = getTranslation(lang, 'stackSection');
+  const theme = useTheme();
 
   const stacks = [
     {
       title: t('titleNext'),
       description: t('desc_next'),
-      icon: <RiNextjsLine size={24} />,
+      icon: <RiNextjsLine size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titleTypeScript'),
       description: t('desc_ts'),
-      icon: <RiJavascriptFill size={24} />,
+      icon: <RiJavascriptFill size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titleNodejs'),
       description: t('desc_nodejs'),
-      icon: <SiNodedotjs size={24} />,
+      icon: <SiNodedotjs size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titleJava'),
       description: t('desc_java'),
-      icon: <RiJavaFill size={24} />,
+      icon: <RiJavaFill size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titleFigma'),
       description: t('desc_figma'),
-      icon: <SiFigma size={24} />,
+      icon: <SiFigma size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titleGCP'),
       description: t('desc_gcp'),
-      icon: <SiGooglecloud size={24} />,
+      icon: <SiGooglecloud size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titlePostgreSQL'),
       description: t('desc_postgresql'),
-      icon: <SiPostgresql size={24} />,
+      icon: <SiPostgresql size={24} color={theme.theme.commonColors.pink} />,
     },
     {
       title: t('titleGIT'),
       description: t('desc_git'),
-      icon: <SiGit size={24} />,
+      icon: <SiGit size={24} color={theme.theme.commonColors.pink} />,
     },
   ];
 
