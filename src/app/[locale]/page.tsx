e498f0dd903870df7c86b92f69427e04416fccb1
heloisacrisilva <heloisacristovao02@gmail.com>
2026-04-06 16:27:18 -0300

    feat: contact section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,16 +5,20 @@ import * as S from './style';
 import { Header } from '@/components/Header';
 import { StackSection } from '@/components/Sections/stackSection';
 import { ProjectsSection } from '@/components/Sections/projectsSection';
+import { ContactSection } from '@/components/Sections/contactSection';
 
 export default function Home({ params }: { params: { locale: string } }) {
   const { locale: lang } = params;
 
   return (
-    <S.Container>
+    <>
       <Header lang={lang} />
-      <AboutSection lang={lang} />
-      <StackSection lang={lang} />
-      <ProjectsSection lang={lang}/>
-    </S.Container>
+      <S.Container>
+        <AboutSection lang={lang} />
+        <StackSection lang={lang} />
+        <ProjectsSection lang={lang}/>
+        <ContactSection lang={lang} />
+      </S.Container>
+    </>
   );
 }
