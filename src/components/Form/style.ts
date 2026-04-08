0da59927287231020f742ba78f29181f0ff2e219
heloisacrisilva <heloisacristovao02@gmail.com>
2026-04-08 10:05:53 -0300

    chore: componentization

---
+++ src/components/Form/style.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const FormsBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Ttile = styled.h1`
+  font-size: ${({ theme }) => theme.fonts.sizer.xxl};
+  color: ${({ theme }) => theme.primaryColor};
+  margin-bottom: 1rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.secondaryBackground};
+  gap: 1rem;
+  width: 100%;
+  max-width: 50%;
+  padding: 4rem;
+  border-radius: 0.5rem;
+`;
+
+export const Input = styled.input`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  border: none;
+  font-size: ${({ theme }) => theme.fonts.sizer.md};
+`;
+
+export const TextArea = styled.textarea`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  font-size: ${({ theme }) => theme.fonts.sizer.md};
+  resize: vertical;
+  border: none;
+`;
+
+export const Button = styled.button`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  font-size: ${({ theme }) => theme.fonts.sizer.md};
+  border: none;
+  background: ${({ theme }) => theme.commonColors.pink};
+  color: ${({ theme }) => theme.commonColors.second};;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 0.2s;
+  margin-top: 1rem;
+  &:hover {
+    background: ${({ theme }) => theme.commonColors.lightPink};
+    color: ${({ theme }) => theme.commonColors.pink};
+  }
+`;
+
--- src/components/Sections/contactSection/index.tsx
+++ src/components/Sections/contactSection/index.tsx
@@ -1,84 +1,16 @@
 'use client';
 
-import { useState } from 'react';
-import { getTranslation } from '@/utils/i18n';
 import * as S from './style';
+import { ContactForm } from '@/components/Form';
 
 interface ContactSectionProps {
   lang: string;
 }
 
 export const ContactSection = ({ lang }: ContactSectionProps) => {
-  const t = getTranslation(lang, 'contactSection');
-  const [form, setForm] = useState({ name: '', email: '', message: '' });
-  const [status, setStatus] = useState<'idle' | 'success' | 'empty' | 'error'>('idle');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
-    if (form.name && form.email && form.message) {
-      setStatus('success');
-      setForm({ name: '', email: '', message: '' });
-    } else if (!form.name || !form.email || !form.message) {
-      setStatus('empty');
-    } else {
-      setStatus('error');
-    }
-  };
-
   return (
     <S.Section id="ContactSection">
-      <S.FormsBox>
-        <S.Form onSubmit={handleSubmit} noValidate>
-          <S.Ttile>{t('title')}</S.Ttile>
-
-          <label htmlFor="name">{t('name')}</label>
-          <S.Input
-            id="name"
-            type="text"
-            name="name"
-            placeholder={t('namePlaceholder')}
-            value={form.name}
-            onChange={handleChange}
-            required
-            aria-required="true"
-          />
-
-          <label htmlFor="email">{t('email')}</label>
-          <S.Input
-            id="email"
-            type="email"
-            name="email"
-            placeholder={t('emailPlaceholder')}
-            value={form.email}
-            onChange={handleChange}
-            required
-            aria-required="true"
-          />
-
-          <label htmlFor="message">{t('message')}</label>
-          <S.TextArea
-            id="message"
-            name="message"
-            placeholder={t('messagePlaceholder')}
-            value={form.message}
-            onChange={handleChange}
-            required
-            aria-required="true"
-            rows={5}
-          />
-
-          <S.Button type="submit">{t('submit')}</S.Button>
-
-          {status === 'success' && <span style={{ color: 'green', marginTop: '1rem' }}>{t('submitSuccess')}</span>}
-          {status === 'empty' && <span style={{ color: 'red', marginTop: '1rem' }}>{t('submitEmpty')}</span>}
-          {status === 'error' && <span style={{ color: 'red', marginTop: '1rem' }}>{t('submitError')}</span>}
-        </S.Form>
-      </S.FormsBox>
+      <ContactForm lang={lang} />
     </S.Section>
   );
 };
